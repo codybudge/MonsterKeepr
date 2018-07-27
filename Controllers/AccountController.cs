6c2106a77150daef891ef97f0a8f348ad3b03029
cody budge <codybudge@gmail.com>
2018-07-27 15:12:22 -0600

    added Register and Login to AccountController

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,11 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using monsterKeepr.Models;
+using monsterKeepr.Repositories;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace monsterKeepr.Controllers
 {
@@ -6,7 +13,65 @@ namespace monsterKeepr.Controllers
   public class AccountController : Controller
   {
 
-private readonly UserRepository _db;
+    private readonly UserRepository _db;
 
+    public AccountController(UserRepository reop)
+    {
+      _db = reop;
+    }
+
+    [HttpPost("register")]
+    public async Task<UserReturnModel> Register([FromBody]RegisterUserModel creds)
+    {
+      if (ModelState.IsValid)
+      {
+        UserReturnModel user = _db.Register(creds);
+        if (user != null)
+        {
+          ClaimsPrincipal principal = user.SetClaims();
+          await HttpContext.SignInAsync(principal);
+          return user;
+        }
+      }
+      return null;
+    }
+    [HttpPost("login")]
+    public async Task<UserReturnModel> Login([FromBody]LoginUserModel creds)
+    {
+      if (ModelState.IsValid)
+      {
+        UserReturnModel user = _db.Login(creds);
+        if (user != null)
+        {
+          ClaimsPrincipal principal = user.SetClaims();
+          await HttpContext.SignInAsync(principal);
+          return user;
+        }
+      }
+      return null;
+    }
+    [HttpGet("authenticate")]
+    public UserReturnModel Authenticate()
+    {
+      var user = HttpContext.User;
+      var id = user.Identity.Name;
+
+      return _db.GetUserById(id);
+    }
+    [Authorize]
+    [HttpPut]
+    public UserReturnModel UpdateAccount([FromBody]UserReturnModel user)
+    {
+      var email =HttpContext.User.Claims.Where(c => c.Type == ClaimTypes.Email)
+        .Select(c =>c.Value).SingleOrDefault();
+      var sessionUser= _db.GetByUserEmail(email);
+
+      if (sessionUser.Id == user.Id)
+      {
+        return _db.UpdateUser(user);
+      }
+      return null;
+    }
+    
   }
 }
