f036580ff85fd2df96b3cfff4b9dfaf106dc086b
cody budge <codybudge@gmail.com>
2018-07-31 15:16:18 -0600

    finished keepController. Delet is empty and does not work

--- Controllers/KeepController.cs
+++ Controllers/KeepController.cs
@@ -1,10 +1,60 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using monsterKeepr.Models;
 
 namespace monsterKeepr
 {
   [Route("api/[controller]")]
   public class KeepController : Controller
-{
-  private readonly KeepRepository db;
-}
+  {
+    private readonly KeepRepository db;
+
+    public KeepController(KeepRepository repo)
+    {
+      db = repo;
+    }
+
+    //GET api/Keep
+    [HttpGet]
+    public IEnumerable<Keep> GetUserKeep()
+    {
+      var userId = HttpContext.User.Identity.Name;
+      return db.GetUserKeep(userId);
+    }
+
+    //GET api/keep/id
+    [HttpGet("{id}")]
+    public Keep Get(int id)
+    {
+      return db.GetById(id);
+    }
+
+    [Authorize]
+    //Post api/keep
+    [HttpPost]
+    public Keep Post([FromBody]Keep newKeep)
+    {
+      var userId = HttpContext.User.Identity.Name;
+      newKeep.userId = userId;
+      if (ModelState.IsValid)
+      {
+        return db.AddKeep(newKeep);
+      }
+      return null;
+    }
+    //PUT api/keep/id
+    [HttpPut("{id}")]
+    public string Put(int id, [FromBody]Keep keep)
+    {
+      db.EditKeep(keep);
+      return "Updated";
+    }
+
+    //DELETE api/keep/id
+    [HttpDelete("{id}")]
+    public void Delet(int id)
+    {
+    }
+  }
 }
