72739e5338a01a0b9f736baf8a21a9447eeadd63
cody budge <codybudge@gmail.com>
2018-07-31 20:12:24 -0600

    finished Vault Controller

---
+++ Controllers/VaultController.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using API_Users.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace monsterKeepr
+{
+  [Route("api/[controller")]
+
+  public class VualtController : Controller
+  {
+    private readonly VaultRepository db;
+    public VualtController(VaultRepository repo)
+    {
+      db = repo;
+    }
+
+    //GET api/vault
+    [Authorize]
+    [HttpGet]
+    public IEnumerable<Vault> Get()
+    {
+      var id = HttpContext.User.Identity.Name;
+      return db.GetAll(id);
+    }
+
+    [Authorize]
+    //POST api/vault
+    [HttpPost]
+    public Vault Post([FromBody]Vault newVualt)
+    {
+      var userId = HttpContext.User.Identity.Name;
+      newVualt.userId = userId;
+      if (ModelState.IsValid)
+      {
+        return db.AddVault(newVualt);
+      }
+      return null;
+    }
+  }
+}
