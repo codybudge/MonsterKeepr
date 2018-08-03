b54bff82f5d5fb4128b853e2acc585c312a5b0a0
cody budge <codybudge@gmail.com>
2018-08-02 19:35:22 -0600

    added all routs to view docs

--- wwwsrc/src/components/Home.vue
+++ wwwsrc/src/components/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="home">
+  <div class="container-fluid Home">
     
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Home',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
--- wwwsrc/src/router/index.js
+++ wwwsrc/src/router/index.js
@@ -1,15 +1,47 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import  Login from '@/components/Login'
+import  Keeps from '@/components/Keeps'
+import  ViewKeep from '@/components/ViewKeep'
+import  Vault from '@/components/Vault'
+import  ViewVault from '@/components/ViewVault'
+import  KeepDetails from '@/components/KeepDetails'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/Home',
+      name: 'Home',
+      component: Home
+    },{
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Login',
+      component: Login
+    },{
+      path: '/Keeps',
+      name: 'Keeps',
+      component: Keeps
+    },{
+      path: '/ViewKeep',
+      name: 'ViewKeep',
+      component: ViewKeep
+    },{
+      path: '/Vault',
+      name: 'Vault',
+      component: Vault
+    },{
+      path: '/ViewVault/:vaultId',
+      name: 'ViewVault',
+      props: true,
+      component: ViewVault
+    },{
+      path: '/KeepDetails/:keepId',
+      name: 'KeepDetails',
+      props: true,
+      component: KeepDetails
+    },
   ]
 })
