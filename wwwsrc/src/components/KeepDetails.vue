846092d02aa81f65e99663f4dedcbcc12d008310
cody budge <codybudge@gmail.com>
2018-08-09 16:17:09 -0600

    added KeepDetails.vue

--- wwwsrc/src/components/KeepDetails.vue
+++ wwwsrc/src/components/KeepDetails.vue
@@ -1,19 +1,34 @@
 <template>
-  <div class="">
-
-  </div>
+    <div class="card" style="width: 18rem;">
+        <h3 class="card-title">{{keep.name}}</h3>
+        <img class="card-img-top" :src="keep.url" alt="Card image">
+        <div class="card-body">
+          <p class="card-text">{{keep.description}}</p>
+          <p class="card-text">Views:{{keep.views}}</p>
+          <p class="card-text">Saves: {{keep.saves}}</p>
+          <a href="#" @click="setKeep(keep)">View Keep</a>
+        </div>
+      </div>
 </template>
 
 <script>
   export default {
-    name: '',
+    name: 'KeepDetails',
     data() {
       return {
 
       }
     },
-    computed: {},
-    methods: {}
+    computed: {
+      keep () {
+        return this.$store.state.currentKeep
+      }
+    },
+    methods: {
+      setKeep() {
+        this.$store.dispatch('setKeep', keep)
+      }
+    }
   }
 
 </script>
