a125ea14f5a68f4170ed433bf612dbbb42a97c35
cody budge <codybudge@gmail.com>
2018-08-11 11:27:24 -0600

    added vault.vue

--- wwwsrc/src/components/Vault.vue
+++ wwwsrc/src/components/Vault.vue
@@ -1,24 +1,62 @@
 <template>
-  <div class="">
-
+  <div class="vaults container-fluid">
+    <div class="row">
+      <div class="col">
+        <div v-if="!currentUser.id">
+          <form @submit.prevent="addNewVault">
+            <input type="text" placeholder="Title" v-model="newVault.name">
+            <input type="text" placeholder="Description" v-model="newVault.description">
+            <button class="btn" type="submit">
+              Submit
+            </button>
+          </form>
+          <div v-for="vault in vaults">
+            <div class="card" style="width: 18rem;">
+              <h3 class="card-title">{{vault.name}}</h3>
+              <div class="card-body">
+                <p class="card-text">{{vault.description}}</p>
+                <router-link :to="{name: 'ViewVault', params: {vaultId: vault.id}}">
+                  Go to Vault
+                </router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import Nav from './Nacv'
   export default {
-    name: '',
+    name: 'Vault',
     data() {
       return {
-
+        newVaut: {
+          Description: ''
+        }
       }
     },
-    computed: {},
-    methods: {}
+    computed: {
+      vaults() {
+        return this.$store.state.vaults
+      },
+      currentUser() {
+        return thus.$store.satate.currentUser
+      }
+    },
+    methods: {
+      addNewVault() {
+        this.$store.dispatch('addNewVault', this.newVault)
+      },
+      setVault() {
+        this.$store.dispatch('setVault', vault)
+      }
+    }
   }
 
 </script>
 
 <style>
-
-
 </style>
