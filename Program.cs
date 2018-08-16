f28ac0a4eff073da88bc2a1b114ccd41416e09c0
cody budge <codybudge@gmail.com>
2018-08-15 19:26:46 -0600

    also cleaned up program file

--- Program.cs
+++ Program.cs
@@ -10,16 +10,17 @@ using Microsoft.Extensions.Logging;
 
 namespace monsterKeepr
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      BuildWebHost(args).Run();
     }
+
+    public static IWebHost BuildWebHost(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build();
+  }
 }
